685b75efd62761f9faabd6fc066e98018afe0bd1
Vitaly Osipov <vi7ali@gmail.com>
2019-01-22 18:19:32 +0300

    Added my substring solution

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,3 +1,18 @@
-def substrings(word, dictionary)
-  
+def substrings(words, dictionary)  
+  ocurrences = {}
+  words_arr = words.delete("!,.'?").downcase.split(" ")
+  words_arr.each do |word|
+    dictionary.each do |dword|
+      if word.include?(dword)
+        if ocurrences[dword].nil?
+          ocurrences[dword] = 1        
+        else
+          ocurrences[dword] += 1
+        end
+      end
+    end
+  end
+  puts ocurrences
 end
+
+substrings("Howdy partner, sit down! How's it going?", ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"])
