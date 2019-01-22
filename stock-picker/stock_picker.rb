ba3c8bc70987c5c1da3bbac3805f70f3fb70fa7a
Vitaly Osipov <vi7ali@gmail.com>
2019-01-22 16:27:32 +0300

    stock-picker assignment complete

---
+++ stock-picker/stock_picker.rb
@@ -0,0 +1,20 @@
+def stock_picker(prices)
+  buy_day, sell_day, best_deal = 0, 0, 0
+  buy_sell_days = []
+  
+  prices.each do |price|
+    if prices.index(price) != 0
+      buy_arr = prices.select {|el| prices.index(el)<prices.index(price)}
+      buy_arr.each do |buy|
+        if price-buy > best_deal
+          best_deal = price-buy
+          buy_sell_days.clear
+          buy_sell_days.push(prices.index(buy), prices.index(price))
+        end
+      end
+    end
+  end
+  puts buy_sell_days.join (",")
+end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
