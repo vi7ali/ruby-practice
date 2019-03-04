961ca55e4786db79ec2eb7ee85f45e34938559d8
Vitaly Osipov <vi7ali@gmail.com>
2019-03-04 13:40:47 +0300

    Merge sort done

---
+++ recursion/merge.rb
@@ -0,0 +1,19 @@
+def merge_sort(list)
+  if list.length <= 1
+    return list
+  else
+    left = merge_sort(list[0...list.length/2])
+    right = merge_sort(list[list.length/2..-1])
+    sorted = []
+    while !left.empty? && !right.empty?
+      if left[0] <= right[0]
+        sorted.push(left[0])
+        left.slice!(0)
+      else
+        sorted.push(right[0])
+        right.slice!(0)
+      end
+    end    
+  end
+  sorted + left + right
+end
