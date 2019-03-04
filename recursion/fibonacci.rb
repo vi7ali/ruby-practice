b655015d2e8019ca502f690ace65b50a00710a5f
Vitaly Osipov <vi7ali@gmail.com>
2019-03-04 11:39:52 +0300

    Fixed the recursive fibonacci bug, where the number less than 2 would blow the stack

--- recursion/fibonacci.rb
+++ recursion/fibonacci.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 def fib(n)
   sequence = [0, 1]
   n.times do
@@ -9,7 +7,5 @@ def fib(n)
 end
 
 def fib_rec(n, sequence=[0,1])
-  n==2?sequence : fib_rec(n-1, sequence.push(sequence[-1] + sequence[-2]))
+  n<=2?sequence : fib_rec(n-1, sequence.push(sequence[-1] + sequence[-2]))
 end
-
-binding.pry
