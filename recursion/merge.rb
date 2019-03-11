0fae7c7e3ef4899d25ec683c4bc594b33b26a1cf
Vitaly Osipov <vi7ali@gmail.com>
2019-03-11 12:48:27 +0300

    Finalized the merge sort recursion excercise

--- recursion/merge.rb
+++ recursion/merge.rb
@@ -1,9 +1,9 @@
 def merge_sort(list)
   if list.length <= 1
     return list
-  else
-    left = merge_sort(list[0...list.length/2])
-    right = merge_sort(list[list.length/2..-1])
+  else    
+    left = merge_sort(list[0...list.length/2])        
+    right = merge_sort(list[list.length/2..-1])    
     sorted = []
     while !left.empty? && !right.empty?
       if left[0] <= right[0]
