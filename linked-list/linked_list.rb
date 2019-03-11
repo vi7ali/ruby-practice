cbe594cd95b4158cc69af099c5d0fcbe8ffe5ded
Vitaly Osipov <vi7ali@gmail.com>
2019-03-11 17:22:33 +0300

    Add all other features (pop, contains?, find, to_s insert_at, remove_at)

--- linked-list/linked_list.rb
+++ linked-list/linked_list.rb
@@ -43,9 +43,10 @@ class LinkedList
   end
 
   def at(index)
+    return if size == 0
     node_at = head
 
-    if index.between?(0,size-1)
+    if check_index(index)
       index.times do
         break if node_at.next_node.nil?
         node_at = node_at.next_node
@@ -54,6 +55,85 @@ class LinkedList
     else
       puts "Invalid index"
     end
+  end
+
+  def pop
+    return if size == 0
+    updated_last_node = self.at(size-2)
+    updated_last_node.next_node = nil
+    self.size -= 1
+  end
+
+  def contains?(value)
+    size.times do |index|
+      current_node = self.at(index)
+      return true if current_node.value == value
+    end
+    false
+  end
+
+  def find(value)
+    index = nil
+    size.times do |idx|
+      current_node = self.at(idx)
+      if current_node.value == value
+        index = idx
+        return index
+      end
+    end
+    index
+  end
+
+  def to_s
+    output = ""
+    if size != 0
+      size.times do |index|
+        current_node = self.at(index)
+        output += "( #{current_node.value} ) -> "
+      end
+      output += "nil"
+    end
+    output
+  end
+
+  def insert_at(index, value)
+    return "Invalid index" if !check_index(index)
+
+    new_node = create_node(value)
+
+    if index == 0
+      self.prepend(value)
+    elsif index == size-1
+      self.append(value)
+    else
+      left_node = at(index-1)
+      right_node = at(index)
+      left_node.next_node = new_node
+      new_node.next_node = right_node
+      self.size += 1
+    end    
+  end
+
+  def remove_at(index)
+    return if size == 0
+    return "Invalid index" if !check_index(index)
+
+    to_delete = at(index)
+    
+    if index == size-1
+      pop
+    elsif index == 0
+      self.head = head.next_node
+    else      
+      left_node = at(index-1)
+      right_node = at(index+1)
+      left_node.next_node = right_node
+      self.size -= 1
+    end    
+  end
+
+  def check_index(index)
+    return index.between?(0,size-1) ? true : false
   end
 
 end
