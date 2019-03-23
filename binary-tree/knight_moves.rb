096b89d209322225781d0aeae9acc448e743e2af
Vitaly Osipov <vi7ali@gmail.com>
2019-03-23 18:14:19 +0300

    Finished Knight's Travails project

--- binary-tree/knight_moves.rb
+++ binary-tree/knight_moves.rb
@@ -1,12 +1,14 @@
 class Knight
 
-  attr_accessor :position, :possible_moves
-  def initialize (position = ["D",4])
+  attr_accessor :position, :possible_moves, :previous_knight
+
+  def initialize (position, previous_knight=nil)
     @position = position
-    @possible_moves = possible_moves(position)
+    @possible_moves = get_moves(position)
+    @previous_knight = previous_knight
   end
 
-  def possible_moves(position)
+  def get_moves(position)
     moves = [
               [(position[0].ord+2).chr, position[1]+1],
               [(position[0].ord+2).chr, position[1]-1],
@@ -24,38 +26,55 @@ class Knight
 
     moves
   end
+
+  def moves_history
+    moves = []
+    local_previous = previous_knight
+    while local_previous != nil
+      moves.push(local_previous.position)
+      local_previous = local_previous.previous_knight
+    end    
+    moves.reverse!
+    moves.push(position)
+  end
 end
 
 class MoveFinder
-  def initialize(start_pos, end_pos)
 
-  end
-end
+  attr_accessor :knight, :end_pos, :moves_set, :start_pos
 
-class Board
-  attr_accessor :board
-
-  def initialize
-    @board = build_board
+  def initialize(start_pos = ["D", 4], end_pos)
+    @start_pos = start_pos
+    @knight = create_knight(start_pos, nil)
+    @end_pos = end_pos
+    @moves_set = find_moves
   end
 
-  def build_board
-    new_board = []
+  def create_knight(position, previous_knight)
+    Knight.new(position, previous_knight)
+  end
 
-    8.times do
-      row = []
-      8.times do
-        row.push(" ")
+  def find_moves
+    queue = []
+    queue.push(knight)
+
+    while !queue.empty?
+      if queue[0].position == end_pos
+        break
+      else
+        queue[0].possible_moves.each do |move|
+          queue.push(create_knight(move, queue[0]))
+        end
+        queue.shift
       end
-      new_board.push(row)
     end
-    new_board
-  end
 
-  def show_board
-    board.each do |row|
-      puts row.join("|")
+    moves = queue[0].moves_history
+    puts "It takes #{moves.length - 1} moves to move from #{start_pos} to #{end_pos} \n"
+    
+    moves.each do |move|
+      puts "#{move}"
     end
-    nil
-  end
+
+  end  
 end
